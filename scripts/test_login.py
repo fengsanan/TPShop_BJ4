b79571ebadf2dc3cc4c5df848e7248c2d21cb200
Zcsff <15101094620@163.com>
2018-08-25 09:59:41 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,16 @@
+from base.base_driver import init_driver
+from page.page import Page
+
+
+class TestLogin:
+
+    def setup(self):
+        self.driver = init_driver()
+        self.page = Page(self.driver)
+
+    def test_login(self):
+        self.page.home.click_mine()
+        self.page.mine.click_login_sign_up()
+        self.page.login.input_username("13800138006")
+        self.page.login.input_password("123456")
+        self.page.login.click_login()
